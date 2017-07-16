4f1085a39e75dd637c09d49cec100722e65eea82
Himani Shah <hjshah2@asu.edu>
2017-07-15 23:46:34 -0400

    initial commit

---
+++ src/config.js
@@ -0,0 +1,5 @@
+import { projects } from 'json!../.firebaserc'
+
+export const apiKey = "AIzaSyB4IjrZ4W7g7w_JtEiPvel9QsTsUT4p4z8"
+export const projectId = projects.default
+export const siteName = "FacultyDashboard"
